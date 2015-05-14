a3da6430f211380881daa20511525722f3fa3953
Jacques de Vos <jacdevos@gmail.com>
2015-05-14 09:24:05 +0200

    Session 3 - navigation and table view added

---
+++ src/contacts/contacts/ContactsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactsViewController.h
+//  contacts
+//
+//  Created by jacdevos on 2015/05/12.
+//  Copyright (c) 2015 nreality. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactsViewController : UIViewController
+
+@end
---
+++ src/contacts/contacts/MyTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  MyTableViewController.h
+//  contacts
+//
+//  Created by jacdevos on 2015/05/13.
+//  Copyright (c) 2015 nreality. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTableViewController : UITableViewController
+
+@end
+
